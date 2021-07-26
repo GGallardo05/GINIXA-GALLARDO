3c331f0c3061a53d89293cc5fbd917b8b65f76e5
GGallardo05 <gyg051998@gmail.com>
2021-07-25 22:11:28 -0500

    sumativa 3

---
+++ SUM#3.c
@@ -0,0 +1,35 @@
+ #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	float a, b, c, d;
+	
+		printf ("introduce el numero entero de a: ");
+		scanf("%d", &a);
+		printf ("introduce el numero entero de b: ");
+		scanf("%d", &b);
+		printf ("introduce el numero entero de c: ");
+		scanf("%d", &c);
+		
+		d= b*b-(4*a*c);
+		
+		if (d>0) {
+			printf ("x1: %.f\n", (-b+ sqrt(d) )/(2*a) );
+			printf ("x2: %.f\n", (-b- sqrt(d) )/(2*a) );	
+		}
+		
+		else if (d==0) {
+			printf ( "x: %.f\n", -b/(2*a) ); 
+		}
+		
+		else {
+			printf ("x1: %+ %.f i\n", -b/(2*a), sqrt(-d)/(2*a) );
+			printf ("x2: %- %.f i\n", -b/(2*a), sqrt(-d)/(2*a) );
+		}
+	
+	return 0;
+}
