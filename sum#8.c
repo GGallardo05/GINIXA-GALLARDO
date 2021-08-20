6959948b7f0f4b5fa622d8288958d182cac39aef
GGallardo05 <gyg051998@gmail.com>
2021-08-19 22:42:20 -0500

    Revert "sumativa10"
    This reverts commit 0e4e8423d77dde4864b2188fed4ccc191f76816d.

--- sum#8.c
+++ sum#8.c
@@ -5,11 +5,16 @@ int main()
   
     int i, numero;
 
+    
+     
+
         printf( "\n   La tabla de multiplicar es:\n", numero );
 		scanf( "%d", &numero );
 
         for ( i = 1 ; i <= 10 ; i++ )
             printf( "\n   %d * %d = %d", i, numero, i * numero );
+
+
 
     return 0;
 }
